858200912ce9190c0bfc8d91adc700eba1b3b549
ohalloranjm <johalloran@protonmail.com>
2024-12-26 14:46:31 -0500

    set up navigation bar

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -3,7 +3,6 @@ import AboutMe from './AboutMe';
 export default function LandingPage() {
   return (
     <>
-      <p>navigation bar</p>
       <p>--</p>
       <p>cyling images</p>
       <p>--</p>
--- react-vite/src/router/Router.jsx
+++ react-vite/src/router/Router.jsx
@@ -1,16 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LandingPage from '../components/LandingPage';
+import Layout from './Layout';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <LandingPage />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <LandingPage />,
+      },
+      {
+        path: '/research',
+        element: <h1>Research</h1>,
+      },
+      { path: '/:pagename', element: <h1>placeholder</h1> },
+    ],
   },
-  {
-    path: '/research',
-    element: <h1>Research</h1>,
-  },
-  { path: '/:pagename', element: <h1>placeholder</h1> },
 ]);
 
 export default router;
