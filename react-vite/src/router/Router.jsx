6475ee6cff35aa1310f9f1e212d4ccccde66c283
ohalloranjm <johalloran@protonmail.com>
2024-12-26 13:52:38 -0500

    set up router

---
+++ react-vite/src/router/Router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <h1>main page</h1>,
+  },
+]);
+
+export default router;
