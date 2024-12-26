43d723b19303339380cab245644bf5df61525519
ohalloranjm <johalloran@protonmail.com>
2024-12-26 15:54:32 -0500

    finished layout for now

--- react-vite/src/components/LandingPage/LandingPage.jsx
+++ react-vite/src/components/LandingPage/LandingPage.jsx
@@ -3,7 +3,6 @@ import AboutMe from './AboutMe';
 export default function LandingPage() {
   return (
     <>
-      <p>--</p>
       <p>cyling images</p>
       <p>--</p>
       <AboutMe />
--- react-vite/src/router/Layout.jsx
+++ react-vite/src/router/Layout.jsx
@@ -1,9 +1,20 @@
-import { Outlet } from 'react-router-dom';
+import { Link, NavLink, Outlet } from 'react-router-dom';
+import './Layout.css';
 
 export default function Layout() {
   return (
     <>
-      <div>Navigation Bar</div>
+      <div id='navbar'>
+        <div>
+          <Link to='/'>Molly O'Halloran</Link>
+        </div>
+        <div>
+          <NavLink to='/research'>Research</NavLink>
+          <NavLink to='/outreach'>Outreach</NavLink>
+          <NavLink to='/mentoring'>Mentoring</NavLink>
+          <NavLink to='/contact'>Contact</NavLink>
+        </div>
+      </div>
       <Outlet />
     </>
   );
