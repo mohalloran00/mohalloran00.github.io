49d05325aa21da744e921f9ad78a76115b08acc1
ohalloranjm <johalloran@protonmail.com>
2024-12-26 17:20:54 -0500

    finished BigNav for now

--- react-vite/src/components/LandingPage/BigNavigation/NavBox.jsx
+++ react-vite/src/components/LandingPage/BigNavigation/NavBox.jsx
@@ -1,10 +1,17 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useNavigate } from 'react-router-dom';
 
 export default function NavBox({ page }) {
+  const navigate = useNavigate();
+  const handleClick = () => navigate(page.URL);
+
   return (
-    <div>
-      {page.icon && <FontAwesomeIcon icon={page.icon} />}
-      {page.name}
+    <div className='big-navbox' onClick={handleClick}>
+      <h3 className='bnb-title'>{page.name}</h3>
+      <div className='bnb-icon'>
+        {page.icon && <FontAwesomeIcon icon={page.icon} />}
+      </div>
+      <div className='bnb-blurb'>{page.blurb ?? null}</div>
     </div>
   );
 }
--- react-vite/src/data/navigation.js
+++ react-vite/src/data/navigation.js
@@ -11,21 +11,27 @@ const navigation = [
     name: 'Research',
     URL: '/research',
     icon: faMicroscope,
+    blurb:
+      'Faucibus interdum nostra netus pellentesque lorem. Laoreet vulputate bibendum dignissim tellus mus; pharetra porta maecenas.',
   },
   {
     name: 'Outreach',
     URL: '/outreach',
     icon: faComments,
+    blurb: 'Euismod morbi purus hac ac convallis auctor sagittis.',
   },
   {
     name: 'Mentoring',
     URL: '/mentoring',
     icon: faChalkboardTeacher,
+    blurb:
+      'Dui condimentum inceptos quam hendrerit egestas sit. Curabitur quis mattis iaculis cursus phasellus suscipit taciti mauris. Sociosqu augue platea elementum ultrices cubilia semper cursus aliquet.',
   },
   {
     name: 'Contact',
     URL: '/contact',
     icon: faEnvelope,
+    blurb: 'Ante elementum lectus parturient posuere massa.',
   },
 ];
 
