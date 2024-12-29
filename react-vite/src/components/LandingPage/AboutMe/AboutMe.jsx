90c279b1dae0b90fbd81ecfb158bc57f155587f1
ohalloranjm <johalloran@protonmail.com>
2024-12-29 10:49:27 -0500

    updated AboutMe css

--- react-vite/src/components/LandingPage/AboutMe/AboutMe.jsx
+++ react-vite/src/components/LandingPage/AboutMe/AboutMe.jsx
@@ -4,8 +4,8 @@ import './AboutMe.css';
 export default function AboutMe() {
   const { aboutMe } = data;
   return (
-    <div className='about'>
-      <div className='about-me'>
+    <div className='about-me'>
+      <div className='about-me-text'>
         {aboutMe.map((paragraph, i) => (
           <p key={i}>{paragraph}</p>
         ))}
