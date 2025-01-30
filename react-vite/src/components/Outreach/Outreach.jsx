2ddd03fcd04e670fe8954c3141d49e69dcd3b272
Joy O'Halloran <johalloran@protonmail.com>
2025-01-30 15:19:35 -0800

    data on page

--- react-vite/src/components/Outreach/Outreach.jsx
+++ react-vite/src/components/Outreach/Outreach.jsx
@@ -1,9 +1,20 @@
+import data from '../../data';
 import './Outreach.css';
+
+const { outreach } = data;
 
 export default function Outreach() {
   return (
     <div className='outreach'>
       <h1 className='center'>Stewardship Day at Nine Mile Canyon</h1>
+      {outreach.images.map((src, i) => (
+        <img src={src} key={i} />
+      ))}
+      <div className='outreach-text'>
+        {outreach.text.map((paragraph, i) => (
+          <p key={i}>{paragraph}</p>
+        ))}
+      </div>
     </div>
   );
 }
