f44b6db8fd55967085045462ddd8b0af25d1b21a
Joy O'Halloran <johalloran@protonmail.com>
2025-01-30 16:23:25 -0800

    finished outreach

--- react-vite/src/components/Outreach/Outreach.jsx
+++ react-vite/src/components/Outreach/Outreach.jsx
@@ -8,18 +8,22 @@ export default function Outreach() {
   return (
     <div className='outreach'>
       <h1 className='center'>Stewardship Day at Nine Mile Canyon</h1>
-      {outreach.images.map(({ src, caption }, i) => (
-        <ImageWithCaption
-          src={src}
-          caption={caption}
-          key={i}
-          className='outreach-fig'
-        />
-      ))}
-      <div className='outreach-text'>
-        {outreach.text.map((paragraph, i) => (
-          <p key={i}>{paragraph}</p>
-        ))}
+      <div className='outreach-content'>
+        <div className='outreach-figs'>
+          {outreach.images.map(({ src, caption }, i) => (
+            <ImageWithCaption
+              src={src}
+              caption={caption}
+              key={i}
+              className='outreach-fig'
+            />
+          ))}
+        </div>
+        <div className='outreach-text'>
+          {outreach.text.map((paragraph, i) => (
+            <p key={i}>{paragraph}</p>
+          ))}
+        </div>
       </div>
     </div>
   );
