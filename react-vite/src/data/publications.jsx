6ddce0872b3145dbdc2d380d9b95ae6a26b12aa0
Joy O'Halloran <johalloran@protonmail.com>
2025-02-25 16:25:03 -0800

    added fake publications data

---
+++ react-vite/src/data/publications.jsx
@@ -0,0 +1,18 @@
+const publicationsData = {
+  blurb: 'Lorem ipsum whatever',
+  refereedPublications: [
+    {
+      unanchored: (
+        <>
+          Some guy and <strong>M. O'Halloran</strong>, in the{' '}
+          <em>Journal of Lorem Ipum</em>. 2020.
+        </>
+      ),
+      anchored: '20351354385435135135',
+      URL: 'http://google.com',
+    },
+  ],
+  publicationsInProgress: [],
+};
+
+export default publicationsData;
