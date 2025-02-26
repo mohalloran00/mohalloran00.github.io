f35f682b1d6e40ef2cce9759dde605c2ebae7966
Joy O'Halloran <johalloran@protonmail.com>
2025-02-25 16:32:30 -0800

    better fake publications data

--- react-vite/src/data/index.js
+++ react-vite/src/data/index.js
@@ -1,6 +1,7 @@
 import { cyclingImages, aboutMe } from './homepage.json';
 import navigation from './navigation';
+import publicationsData from './publications';
 import { text, images } from './outreach.json';
 const outreach = { text, images };
-const data = { cyclingImages, aboutMe, navigation, outreach };
+const data = { cyclingImages, aboutMe, navigation, outreach, publicationsData };
 export default data;
--- react-vite/src/data/publications.jsx
+++ react-vite/src/data/publications.jsx
@@ -2,13 +2,17 @@ const publicationsData = {
   blurb: 'Lorem ipsum whatever',
   refereedPublications: [
     {
-      unanchored: (
-        <>
-          Some guy and <strong>M. O'Halloran</strong>, in the{' '}
-          <em>Journal of Lorem Ipum</em>. 2020.
-        </>
-      ),
-      anchored: '20351354385435135135',
+      authors: 'O’Halloran, Molly M.',
+      year: 2020,
+      title: 'Research about rivers',
+      journal: 'In: Journal of Rocks, no. 84',
+      URL: 'http://google.com',
+      URLtext: '2342304',
+    },
+    {
+      authors: 'Me and some other guy',
+      title: 'Even more research about rivers',
+      journal: 'Journal of Rivers (2025)',
       URL: 'http://google.com',
     },
   ],
